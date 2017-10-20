016e7c8ac7ed50477c1d9d5fcc9e41e525667db6
Piyush Santwani <piyush.santwani@bookmyshow.com>
2017-10-20 14:53:33 +0530

    reviewing test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -9,26 +9,30 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
+//Setting up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = _$(window); //$ is going to try using the browser's window. But there isnt one, hence we explicitly tell it to use the global window we have defined.
 
 chaiJquery(chai, chai.util, $);
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
+
+  //Wherever use JSX, you have to import React.
+
   const componentInstance =  TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
+      <ComponentClass {...props} /> 
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  return $(ReactDOM.findDOMNode(componentInstance)); //produces HTML
 }
 
 $.fn.simulate = function(eventName, value) {
   if (value) {
-    this.val(value);
+    this.val(value); //this is reference to selected HTML element that we want to perform an action upon.
   }
   TestUtils.Simulate[eventName](this[0]);
 };
